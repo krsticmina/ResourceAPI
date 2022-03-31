a1bd013740c12aabd075446f2c5df8bfae4c5798
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 11:28:59 +0200

    Added Employee Entity

--- ResourceAPI/Entities/Employee.cs
+++ ResourceAPI/Entities/Employee.cs
@@ -4,18 +4,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ResourceAPI.Entities
 {
     public class Employee
-    {   
+    {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
         [Required]
         [MaxLength(100)]
         public string Name { get; set; }
+        [Required]
         [EmailAddress]
-        public string? Email { get; set; }
+        public string Email { get; set; } = string.Empty;
+        [Required]
         [Phone]
-        public string? PhoneNumber { get; set; }
-
+        public string PhoneNumber { get; set; } = string.Empty;
+        public int ManagerId { get; set; }
+        [Required]
+        public int Salary { get; set; }
         public Employee(string name)
         {
             Name = name;
