d301b6ef7e12d4ce7ab34ae3231fc22a04856bf9
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 14:23:07 +0200

    Implemented Mapping

--- ResourceAPI/Profiles/EmployeeProfile.cs
+++ ResourceAPI/Profiles/EmployeeProfile.cs
@@ -4,6 +4,13 @@ namespace ResourceAPI.Profiles
 {
     public class EmployeeProfile : Profile
     {
+        public EmployeeProfile() 
+        {
+            CreateMap<Entities.Employee, Models.EmployeeDto>();
+            CreateMap<Models.EmployeeDto, Entities.Employee>();
+            CreateMap<Models.EmployeeForInsertionDto, Entities.Employee>().ReverseMap();
+
+        }
 
     }
 }
