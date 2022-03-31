20691a90746208308cf15e1e862301b5a58984b2
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 11:31:25 +0200

    Added DbContext

--- ResourceAPI/DbContexts/ResourceDbContext.cs
+++ ResourceAPI/DbContexts/ResourceDbContext.cs
@@ -9,11 +9,26 @@ namespace ResourceAPI.DbContexts
         {
         }
 
-        DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<Employee>().HasData(
+               new Employee("Jana Filipovic")
+               {
+                   Id = 1,
+                   Email = "jf@gmail.com",
+                   PhoneNumber = "+38167543293",
+                   Salary = 1000
+               },
+               new Employee("Ema Vuckovic")
+               {
+                   Id = 2,
+                   Email = "ev@gmail.com",
+                   PhoneNumber = "+38167235735",
+                   Salary = 500
+               }
+               );
             base.OnModelCreating(modelBuilder);
         }
     }
