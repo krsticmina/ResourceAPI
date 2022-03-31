13e5e421f279b144b82222b09314c4b83a191eab
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 11:32:52 +0200

    Updated EmployeeDto

--- ResourceAPI/Models/EmployeeDto.cs
+++ ResourceAPI/Models/EmployeeDto.cs
@@ -1,11 +1,21 @@
-﻿namespace ResourceAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceAPI.Models
 {
     public class EmployeeDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(100)]
+        public string PhoneNumber { get; set; } = string.Empty;
+        [Required]
+        public int Salary { get; set; }
 
     }
 }
