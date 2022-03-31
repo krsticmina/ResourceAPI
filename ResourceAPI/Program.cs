ce8d13695a1c5acf501d5d72ea2421d5f5f87301
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 14:24:58 +0200

    Updated Program class

--- ResourceAPI/Program.cs
+++ ResourceAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ResourceAPI.DbContexts;
+using ResourceAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
