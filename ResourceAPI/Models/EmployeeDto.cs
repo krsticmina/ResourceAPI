898efcffb25e206d850e2ced9b702d0a84c3db2b
Mina Krstic <Mina.Krstic@endava.com>
2022-03-31 11:29:15 +0200

    Added Employee Dtos

--- ResourceAPI/Models/EmployeeDto.cs
+++ ResourceAPI/Models/EmployeeDto.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
-        public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
 
     }
 }
---
+++ ResourceAPI/Models/EmployeeForInsertionDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResourceAPI.Models
+{
+    public class EmployeeForInsertionDto
+    {
+        [Required(ErrorMessage = "You should provide a name value.")]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "You should provide an email value.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "You should provide a phone number value.")]
+        [MaxLength(100)]
+        public string PhoneNumber { get; set; } = string.Empty;
+        public int ManagerId { get; set; }
+
+        [Required]
+        public int Salary { get; set; }
+    }
+}
